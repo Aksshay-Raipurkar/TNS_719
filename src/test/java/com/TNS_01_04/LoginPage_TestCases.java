60e99bfe479129bd892ec6903358df4b76849b9f
AR <AR@Ideapad-310>
2025-04-02 11:14:30 +0530

    New Build

--- src/test/java/com/TNS_01_04/LoginPage_TestCases.java
+++ src/test/java/com/TNS_01_04/LoginPage_TestCases.java
@@ -1,14 +1,26 @@
 package com.TNS_01_04;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class LoginPage_TestCases {
+	@Test
 
 	void testValidData() {
+
+		Assert.assertEquals(3, 3);
 	}
 
+	@Test
 	void testInValidData() {
+
+		Assert.assertEquals(5, 5);
 	}
 
+	@Test
 	void testHeader() {
+
+		Assert.assertEquals(6, 6);
 	}
 
 }
